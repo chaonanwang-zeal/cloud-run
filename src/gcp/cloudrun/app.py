79de5feb58a848a4d5cafcfc6c1f3cd655f91428
chaonanwang <chaonan.wang@zdh.co.jp>
2024-07-05 15:18:49 +0900

    added

--- src/gcp/cloudrun/app.py
+++ src/gcp/cloudrun/app.py
@@ -1,17 +1,51 @@
+# Copyright 2020 Google, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START eventarc_http_quickstart_server]
+# [START eventarc_audit_storage_server]
 import os
-import csv
+
+from cloudevents.http import from_http
+
 from flask import Flask, request
-from google.cloud import bigquery, storage
 
 app = Flask(__name__)
-client = bigquery.Client()
-storage_client = storage.Client()
+# [END eventarc_audit_storage_server]
+# [END eventarc_http_quickstart_server]
 
-@app.route('/', methods=['POST'])
+
+# [START eventarc_http_quickstart_handler]
+# [START eventarc_audit_storage_handler]
+@app.route("/", methods=["POST"])
 def index():
-    data = request.get_json()
+    # Create a CloudEvent object from the incoming request
+    event = from_http(request.headers, request.data)
+    # Gets the GCS bucket name from the CloudEvent
+    # Example: "storage.googleapis.com/projects/_/buckets/my-bucket"
+    bucket = event.get("subject")
 
-    print(f"Received data: {data}、これはデータです。")  # 记录接收到的数据
+    print(f"Detected change in Cloud Storage bucket: {bucket}")
+    return (f"Detected change in Cloud Storage bucket: {bucket}", 200)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+# [END eventarc_audit_storage_handler]
+# [END eventarc_http_quickstart_handler]
+
+
+# [START eventarc_http_quickstart_server]
+# [START eventarc_audit_storage_server]
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# [END eventarc_audit_storage_server]
+# [END eventarc_http_quickstart_server]
